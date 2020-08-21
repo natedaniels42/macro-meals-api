7b7ad6a000d866eb450b02b0ca03ef37597daa06
Nathan Daniels <nathandanielsbassoon@gmail.com>
2020-08-21 16:31:16 -0500

    Added cors and bodyparser to server.js

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT;
+
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    methods: 'GET,POST,PUT,DELETE',
+    optionsSuccessStatus: 200
+}));
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
