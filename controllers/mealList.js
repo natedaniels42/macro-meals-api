b2017bd04f16c4f5e7853dc7a875b1a46fdb875a
Nathan Daniels <nathandanielsbassoon@gmail.com>
2020-08-25 14:22:11 -0500

    meallist index working

--- controllers/mealList.js
+++ controllers/mealList.js
@@ -1,10 +1,29 @@
 const db = require('../models');
 
+const index = (req, res) => {
+    db.MealList.find({}, (err, foundMealList) => {
+        if (err) console.log('Error in meallist index: ', err);
+
+        res.status(200).json(foundMealList);
+    });
+};
+
 const create = (req, res) => {
     db.MealList.create(req.body, (err, savedMealList) => {
         if (err) console.log('Error in meal list create: ', err);
 
         res.status(200).json(savedMealList);
+    });
+};
+
+const update = (req, res) => {
+    db.MealList.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedMealList) => {
+        if (err) console.log('Error in meallist update: ', err);
+
+        if (!updatedMealList) {
+            res.status(400).json({message: `Could not find meallist with id ${req.params.id}`});
+        }
+        res.json(updatedMealList);
     });
 };
 
@@ -17,6 +36,8 @@ const destroy = (req, res) => {
 };
 
 module.exports = {
+    index,
     create,
+    update,
     destroy,
 };
--- routes/mealList.js
+++ routes/mealList.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const ctrl = require('../controllers');
 
+router.get('/', ctrl.mealList.index);
 router.post('/', ctrl.mealList.create);
+router.put('/:id', ctrl.mealList.update);
 router.delete('/:id', ctrl.mealList.destroy);
 
 module.exports = router;
